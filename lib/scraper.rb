823cc39b308d3fdb74247f9ab65c2bb3c19036b8
Nicolas Marini <nmarini44@gmail.com>
2018-07-06 00:11:29 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
 
       doc.css(".post").each do |post|
         course = Course.new
-        course.title = post.css("h2").text
+        course.title = 
         course.schedule = post.css(".date").text
         course.description = post.css("p").text
       end
